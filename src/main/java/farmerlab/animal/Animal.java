84a60e4a67228bbd9c876e1a4a4f2fc80a9d6147
(Jordy) Jordan Mitchell <83673134+Genbuos@users.noreply.github.com>
2023-10-24 13:22:00 -0400

    Update Animal.java
    clean up

--- src/main/java/farmerlab/animal/Animal.java
+++ src/main/java/farmerlab/animal/Animal.java
@@ -1,8 +1,8 @@
 package farmerlab.animal;
 
-<<<<<<< HEAD
+
 public class Animal {
-=======
+
 import farmerlab.interfaces.Eater;
 import farmerlab.interfaces.NoiseMaker;
 
@@ -28,6 +28,6 @@ public abstract class Animal implements NoiseMaker, Eater {
     public void eat(Edible food){
         System.out.println(animalType + "is eating " + food.getName());
     }
->>>>>>> c2fabe88d1ac1e8ec293287f8371ed42bb126ebb
+
 
 }
