e2a3e8fe88155f1545c4a229a7037a6dd4396cb8
kcj100 <kaljohnson2003@gmail.com>
2023-10-27 15:55:27 -0400

    add day test

---
+++ src/test/java/farmerlab/WeeklyScheduleTest.java
@@ -0,0 +1,30 @@
+package farmerlab;
+
+import farmerlab.farm.Farm;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WeeklyScheduleTest {
+
+    @Test
+    void sunday() {
+    }
+
+    @Test
+    void monday() {
+    }
+
+    @Test
+    void tuesday() {
+    }
+
+    @Test
+    void morningRideTest() {
+    }
+
+    @Test
+    void morningBreakfastTest() {
+    }
+}
