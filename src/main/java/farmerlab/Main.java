6f6779cb4c92f545f0b10784092b8c577fa60daa
Yaw <baffourampadu06@gmail.com>
2023-10-29 18:13:35 -0400

    Add all changes

--- src/main/java/farmerlab/Main.java
+++ src/main/java/farmerlab/Main.java
@@ -1,10 +1,10 @@
 package farmerlab;
 
+import farmerlab.farm.CropRow;
 import farmerlab.farm.Plot;
 
 public class Main {
     public static void main(String[] args) {
-        Plot plot = new Plot();
 
     }
 }
--- src/test/java/farmerlab/DayTest.java
+++ src/test/java/farmerlab/DayTest.java
@@ -1,13 +1,19 @@
 package farmerlab;
 
 import farmerlab.animal.Horse;
-import farmerlab.farm.CropRow;
-import farmerlab.farm.Farm;
-import farmerlab.farm.Plot;
-import farmerlab.farm.Stable;
+import farmerlab.crop.CornStalk;
+import farmerlab.crop.Crop;
+import farmerlab.crop.TomatoPlant;
+import farmerlab.farm.*;
+import farmerlab.interfaces.Rideable;
+import farmerlab.person.Farmer;
+import farmerlab.person.Pilot;
+import farmerlab.vehicle.CropDuster;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class DayTest {
 Plot plot = new Plot();
@@ -27,7 +33,32 @@ Plot plot = new Plot();
 
     @Test
     void monday() {
-        Plot plot = new Plot();
+
+        CropRow cropRow = new CropRow();
+        CropRow cropRow1 = new CropRow();
+        Crop Tomatoplant = new TomatoPlant();
+        Crop CornStalk = new CornStalk();
+        CropDuster cropDuster = new CropDuster(1, "wiiiinngg");
+        Field field = new Field();
+        Pilot froilanda = new Pilot("Dawn", "geronimo", 123);
+
+        cropRow.addCrop(Tomatoplant);
+        field.addCropRow(cropRow);
+
+        cropRow1.addCrop((CornStalk));
+        field.addCropRow(cropRow);
+
+        froilanda.mount(cropDuster);
+        cropDuster.setInUse(true); //It should be assumed that it is in use when cropDuster is mounted.
+        cropDuster.fly(field);
+
+
+        assertTrue(field.getCropRows().get(0).getCropRow().get(0).isHasBeenFertilized());
+        assertTrue(field.getCropRows().get(1).getCropRow().get(0).isHasBeenFertilized());
+
+
+
+
 
     }
 
