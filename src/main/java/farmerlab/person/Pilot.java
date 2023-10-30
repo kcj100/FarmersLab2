fac6b36efe098dc846aaed43dbaf67525d0114cd
DSavag3 <dantedsavage@gmail.com>
2023-10-29 18:23:05 -0700

    Eight Push

--- src/main/java/farmerlab/person/Pilot.java
+++ src/main/java/farmerlab/person/Pilot.java
@@ -56,7 +56,7 @@ private boolean isRiding;
         if (ride instanceof FarmVehicle) {
 
             if (ride instanceof CropDuster) {
-                if (!ride.isInUse()) {
+                if (ride.isInUse()!=true) {
                     ride.setInUse(true);
                     System.out.println("The ride is mounted");
 
@@ -83,7 +83,9 @@ private boolean isRiding;
         if (ride instanceof FarmVehicle) {
 
             if (ride instanceof CropDuster) {
-                if (ride.isInUse()) {
+                if (ride.isInUse()!=false
+
+                ) {
                     ride.setInUse(false);
                     System.out.println("The ride is dismounted");
 
